06fd303f7af683fe72a05f4b4a6f493d6a10acbe
DiegoVV <dvvolkmann@gmail.com>
2022-12-13 06:09:52 -0300

    Implemented loading spinner while data is gathered from api

--- src/App.js
+++ src/App.js
@@ -8,25 +8,25 @@ export default function App() {
 
 	const [usersData, setUsersData] = useState([]);
 	const [teamsData, setTeamsData] = useState([]);
+	const [loading, setLoading] = useState(99);
 
 	// ----------------------------------------------------------------------------------------------------------
 	// GET API INFO
 	React.useEffect(() => {
+		setLoading(2);
 		axios.get("https://cgjresszgg.execute-api.eu-west-1.amazonaws.com/users/").then((response) => {
 			// console.log(response.data);
 			setUsersData(response.data);
+			setLoading((l) => l - 1);
 		});
 		axios.get("https://cgjresszgg.execute-api.eu-west-1.amazonaws.com/teams/").then((response) => {
 			// console.log(response.data);
 			setTeamsData([].concat(response.data).sort((a, b) => (a.name > b.name ? 1 : -1)));
+			setLoading((l) => l - 1);
 		});
 	}, []);
 
 	// ----------------------------------------------------------------------------------------------------------
 	// RENDER
-	return (
-		<div className="App">
-			<TeamsOverview teamsData={teamsData} />
-		</div>
-	);
+	return <div className="App">{loading > 0 ? <div className="loadingSpinner"></div> : <TeamsOverview teamsData={teamsData} />}</div>;
 }
